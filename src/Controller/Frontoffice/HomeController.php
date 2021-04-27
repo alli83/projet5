9b858a88ca32de2d910c67315c8347d2668e4b15
allison guilhem <allison.guilhem@gmail.com>
2021-04-27 06:32:39 +0200

    template home + css

--- src/Controller/Frontoffice/HomeController.php
+++ src/Controller/Frontoffice/HomeController.php
@@ -4,18 +4,19 @@ declare(strict_types=1);
 
 namespace  App\Controller\Frontoffice;
 
-use App\Service\Http\Request;
+use App\Controller\ControllerInterface\ControllerInterface;
+use App\Service\ErrorsHandlers\Errors;
 use App\Service\Http\Response;
 use App\View\View;
 use App\Service\Http\Session\Session;
 use App\Service\Utils\File;
 use App\Service\Utils\Mailer;
+use App\Service\Utils\Validity;
 
-final class HomeController
+final class HomeController implements ControllerInterface
 {
     private View $view;
     private Session $session;
-
 
     public function __construct(View $view, Session $session)
     {
@@ -28,24 +29,34 @@ final class HomeController
         return new Response($this->view->render(['template' => 'accueil']));
     }
 
-    public function contactDev(Request $request): Response
+    public function contactDev(?object $request): Response
     {
-        $request = $request->request()->all();
-        $message = new Mailer();
-        $message->sendMessageContact("frontoffice/mail/contactAdmin.html.twig", $request);
-        $this->session->addFlashes('success', 'Votre message a été envoyé');
+        if ($request !== null) {
+            $request = $request->all();
+
+            $validityTools = new Validity();
+            $request = $validityTools->validityVariables($request);
+
+            $message = new Mailer();
+            $contact = $message->sendMessageContact("frontoffice/mail/contactAdmin.html.twig", $request);
+            if ($contact === false) {
+                $this->session->addFlashes('danger', 'Nous sommes désolé mais votre message n\'a pas pu être envoyé');
+            } elseif ($contact === true) {
+                $this->session->addFlashes('success', 'Votre message a bien été envoyé');
+            }
+        }
         return new Response($this->view->render(['template' => 'accueil', 'data' => []]));
     }
 
-    public function getCv(string $fileName): Response
+    public function getCv(array $fileName): Response
     {
-        $fileToDownload = new File($fileName);
+        $fileToDownload = new File(htmlspecialchars($fileName["file"]));
         $result = $fileToDownload->downloadFile();
 
-        if ($result !== false) {
-            // TO DO Response
-            return new Response("fichier téléchargé", 200);
+        if ($result === false) {
+            $error = new Errors(404);
+            return $error->handleErrors();
         }
-        return new Response("une erreur est survenue", 404);
+        return new Response("fichier téléchargé", 200);
     }
 }

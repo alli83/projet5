adaa018f144e86d8dceaa8e11787c01b19a18062
allison guilhem <allison.guilhem@gmail.com>
2021-04-27 06:19:27 +0200

    load controllers and repositories

--- public/index.php
+++ public/index.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 require_once '../vendor/autoload.php';
 
+use App\Service\Container;
 use App\Service\Router;
 use App\Service\Http\Request;
 
@@ -15,7 +16,9 @@ if (APP_ENV === 'dev') {
     $whoops->register();
 }
 
+$container = new Container();
 $request = new Request($_GET, $_POST, $_FILES, $_SERVER);
-$router = new Router($request);
+$router = new Router($request, $container);
+
 $response = $router->run();
 $response->send();

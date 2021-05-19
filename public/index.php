2c8c0500e26bca6863c17d9f0504b7a7d5a96798
allison guilhem <allison.guilhem@gmail.com>
2021-05-19 07:32:50 +0200

    Db structure with examples + corr. typos

--- public/index.php
+++ public/index.php
@@ -12,9 +12,9 @@ use App\Service\Utils\ServiceProvider;
 const APP_ENV = 'dev';
 
 if (APP_ENV === 'dev') {
-       // $whoops = new \Whoops\Run();
-       // $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler());
-       // $whoops->register();
+       $whoops = new \Whoops\Run();
+       $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler());
+       $whoops->register();
 }
 
 $serviceProvider = new ServiceProvider();
